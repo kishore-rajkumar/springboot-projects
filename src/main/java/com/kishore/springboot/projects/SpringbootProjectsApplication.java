e238b17220b84948d418bcdeec116e20aac02bf0
Kishore Rajkumar <kishore.rajkumar@hotmail.com>
2022-01-12 20:08:43 +0530

    base main project

---
+++ src/main/java/com/kishore/springboot/projects/SpringbootProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.springboot.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kishore/springboot/projects/SpringbootProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.springboot.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
