6d70a5d7e30f0d4e268db90eb9de2206b94337f7
Kishore Rajkumar <kishore.rajkumar@hotmail.com>
2022-01-14 19:39:16 +0530

    code for jpa update and retrieve added

---
+++ src/main/java/com/kishore/springboot/projects/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.kishore.springboot.projects;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+	Customer findById(long id);
+
+	List<Customer> findByLastName(String lastName);
+
+}
